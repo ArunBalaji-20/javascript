965251bf957ba7b46073168ce48cc62c799ad969
Arun Balaji <arunbalaji50@gmail.com>
2025-04-04 13:48:11 +0530

    edited task-6

--- Task-6/script.js
+++ Task-6/script.js
@@ -13,7 +13,13 @@ list.addEventListener("dragover", (e) => {  //fires repeatedly
     e.preventDefault();
     const draggingItem = document.querySelector(".dragging");
     const afterElement = [...list.children].find(item => 
-        e.clientY < item.getBoundingClientRect().top + item.offsetHeight / 2
+        e.clientY < item.getBoundingClientRect().top + item.offsetHeight / 2  
     );
     list.insertBefore(draggingItem, afterElement);
 });
+
+
+// item.getBoundingClientRect().top: The Y-coordinate of the item's top.
+// item.offsetHeight / 2: Half of the item's height.
+//offsetHeight - The offsetHeight property returns the viewable height of an element (in pixels), including padding, border and scrollbar, but not the margin
+//getBoundingClientRect - Return the size of an element and its position relative to the viewport
